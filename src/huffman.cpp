4b4fba122931da3f87809ccce840026ab569a354
CJ Balmaceda <cbalmaceda40@gmail.com>
2026-08-27 17:47:47 -0400

    finished writing bitstream.cpp which is incharge of doing bit related operations, also just finished writing build_frequency_table() which as the func name suggests builds out the frequency table

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -1,5 +1,5 @@
 #include "huffman.h"
-
+#include <map>
 #include <queue>
 
 // TODO: implement each function declared in huffman.h.
@@ -10,3 +10,12 @@
 //   - build_code_table: walk the tree root-to-leaf, left=0, right=1,
 //     accumulating (bits, width) per byte value.
 //   - huffman_encode/huffman_decode must round-trip exactly.
+
+
+std::map<uint8_t, int> build_frequency_table(const std::vector<uint8_t>& data){
+    std::map<uint8_t, int> frequency_table;
+    for(size_t i=0; i<data.size(); i++){
+        frequency_table[data[i]]++;
+    }
+    return frequency_table;
+}
